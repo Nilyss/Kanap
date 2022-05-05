d7959372388c263f2ab5981e8d8a4070066da45f
Nicolas DECRESSAC <decressac.nicolas@gmail.com>
2022-05-05 16:46:49 +0200

    création dossier js. Ajout dossier & fichier S.O.L.I.D 'Single responsibility principle'. Creation fonction ver.1 récupération de l'API et intégration des produits dans le DOM de la page d'acceuil

---
+++ front/js/script.js
@@ -0,0 +1,39 @@
+// adresse de l'API
+let url = "http://localhost:3000/api/products";
+
+// Remplissage du DOM via API fetch
+
+async function displayProducts () {
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        const productsList = data;
+        console.log(productsList);
+        for (product of productsList) {
+
+            const itemLink = 
+                document.createElement("a")
+                document.querySelector(".items")
+                // itemLink = `href= "#${""}"`
+                .appendChild(itemLink);
+            const itemArticle = 
+                document.createElement("article")
+                itemLink.appendChild(itemArticle);
+            const itemImg =
+                document.createElement("img")
+                itemImg.src = product.imageUrl
+                itemImg.alt = product.altText
+                itemArticle.appendChild(itemImg);
+            const itemTitle = 
+                document.createElement("h3")
+                itemTitle.innerText = product.name
+                itemArticle.appendChild(itemTitle);
+            const itemDescription =
+                document.createElement("p")
+                itemDescription.innerText = product.description 
+                itemArticle.appendChild(itemDescription);            
+        }
+    })
+    return displayProducts;
+};
+console.log(displayProducts())
